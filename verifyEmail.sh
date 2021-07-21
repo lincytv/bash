2ea8bfd3a4be4b5a49fc0380c06d3788ec8953c6
Lincy Varghese <lincytv@gmail.com>
2021-07-21 21:08:44 +0530

    Update verifyEmail.sh

--- verifyEmail.sh
+++ verifyEmail.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
  
-regex="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$"
+regex="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
 echo "ENTER THE E-MAIL ADDRESS"
 read i
  
-if [[ $i =~ $regex ]] ; then
+if [[ "$i" =~ $regex ]] ; then
     echo "OK"
 else
     echo "not OK"
 fi
- 
